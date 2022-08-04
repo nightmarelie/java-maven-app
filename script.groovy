140453c38d3faf89ead096ca80bb67159d13af1d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-04 15:38:01 -0600

    fix: docker login

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build . -t 159.223.162.80:8083/java-maven-app:jma-2.0'
-        sh "echo $PASS | docker login -u $USERNAME --password-stdin 159.223.162.80:8083"
+        sh "echo $PASS | docker login -u $USER --password-stdin 159.223.162.80:8083"
         sh 'docker push 159.223.162.80:8083/java-maven-app:jma-2.0'
     }
 } 
