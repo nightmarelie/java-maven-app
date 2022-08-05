58cab9150f6630f7ede5ec1d8b4d9454ebeb19cd
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-04 22:11:31 -0600

    chore: test webhook

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "building the docker image...."
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build . -t 159.223.162.80:8083/java-maven-app:jma-2.0'
         sh "echo $PASS | docker login -u $USER --password-stdin 159.223.162.80:8083"
